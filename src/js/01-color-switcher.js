2317f6a1cafae5bcc58e44687210df572df8c7bd
Valentin Sachenko <valentin181988@gmail.com>
2021-10-30 19:59:08 +0300

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timeoutId = null;
+
+start.addEventListener('click', () => {
+
+     timeoutId = setInterval(() => {
+     const newColor = getRandomHexColor();
+     body.style.backgroundColor = newColor;
+}, 1000);
+});
+
+stop.addEventListener('click', () => {
+clearInterval(timeoutId)
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 
